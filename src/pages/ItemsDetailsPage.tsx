ae72dbecb2a462f9266a78d482040d8993828092
glyjoo <gsalvaloza@usa.edu.ph>
2026-03-07 00:04:38 +0800

    feat: Implement farmer review functionality and establish core application pages with comprehensive routing.

--- src/pages/ItemsDetailsPage.tsx
+++ src/pages/ItemsDetailsPage.tsx
@@ -1,6 +1,6 @@
 import ItemSection from "../components/shop/ItemSection";
 import RelatedProducts from "../components/shop/RelatedProducts";
-import ItemReview from "../components/shop/ItemReview";
+import ItemReview from "../components/reviews/ItemReview";
 
 export default function ItemsDetailsPage() {
     return (
